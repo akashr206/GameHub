31e9e95d6cc4d85c5bdd3a7912d0028fcd398a10
akashr206 <akashr6514@gmail.com>
2025-06-28 22:05:26 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`${socket.id} connected`)
+})
+
+
+server.listen(9090, () => {
+    console.log("Listening to the port", 9090);
+});
