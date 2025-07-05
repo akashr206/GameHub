3dd6cf04bb71c1264e003870c95cccc577dc4e7d
akashr206 <akashr6514@gmail.com>
2025-07-06 00:04:33 +0530

    remove cors issue

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: process.env.Frontend,
+        origin: "*",
         methods: ["GET", "POST"],
     },
 });
