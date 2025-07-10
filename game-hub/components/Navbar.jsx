f3d27beed48d10d8dee79193297d9f951a4e8145
akashr206 <akashr6514@gmail.com>
2025-07-10 21:55:30 +0530

    update navbar

--- game-hub/components/Navbar.jsx
+++ game-hub/components/Navbar.jsx
@@ -2,8 +2,15 @@ import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
     return (
         <nav className="fixed w-full flex justify-between items-center px-16 h-16 bg-background/60 backdrop-blur-lg">
-            <div>Logo</div>
-            <div>navs</div>
+            <div>Game Hub</div>
+            <nav className="flex gap-2 items-center justify-center">
+                {" "}
+                <ul>
+                    <li>Single Payer Games</li>
+                    <li>Multiplayer Games</li>
+                    <li>About</li>
+                </ul>{" "}
+            </nav>
             <div>
                 <ThemeToggle />
             </div>
